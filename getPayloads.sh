15e3ca0ec137b468062b37e628cf72b3b5a95e11
reverc0de <reverc0de@users.noreply.github.com>
2020-06-02 16:31:23 +0000

    Add files via upload

---
+++ getPayloads.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+### fuzzdb
+git clone https://github.com/fuzzdb-project/fuzzdb.git
+
+### SecLists
+git clone https://github.com/danielmiessler/SecLists.git
+
+### xsuperbug
+git clone https://github.com/xsuperbug/payloads.git
+
+### 7ioSecurity
+git clone https://github.com/7ioSecurity/XSS-Payloads.git
+
+### shadsidd
+git clone https://github.com/shadsidd/Automated-XSS-Finder.git
+
+### tennc
+git clone https://gist.github.com/4026cfd0925aaad0a655.git
+
+### sqlifuzzer
+git clone https://github.com/ContactLeft/sqlifuzzer.git
+
+### wfuzz
+git clone https://github.com/xmendez/wfuzz.git
+
+### xsscx
+git clone https://github.com/xsscx/Commodity-Injection-Signatures.git
+
+### TheRook
+git clone https://github.com/TheRook/subbrute.git
+
+### danielmiessler
+git clone https://github.com/danielmiessler/RobotsDisallowed.git
+
+### swisskyrepo
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
+
+### 1N3
+git clone https://github.com/1N3/IntruderPayloads.git
+
+### cujanovic
+git clone https://github.com/cujanovic/Open-Redirect-Payloads.git
+
+git clone https://github.com/cujanovic/subdomain-bruteforce-list.git
+
+git clone https://github.com/cujanovic/CRLF-Injection-Payloads.git
+
+git clone https://github.com/cujanovic/Virtual-host-wordlist.git
+
+git clone https://github.com/cujanovic/dirsearch-wordlist.git
+
+### lavalamp-
+git clone https://github.com/lavalamp-/password-lists.git
+
+### arnaudsoullie
+git clone https://github.com/arnaudsoullie/ics-default-passwords.git
+
+### scadastrangelove
+git clone https://github.com/scadastrangelove/SCADAPASS.git
+
+### jeanphorn
+git clone https://github.com/jeanphorn/wordlist.git
+
+### j3ers3
+git clone https://github.com/j3ers3/PassList.git
+
+### nyxxxie
+git clone https://github.com/nyxxxie/awesome-default-passwords.git
